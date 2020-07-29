98adf956b3b19b843b13cb37188a091b8893464c
phslima <psenalima@gmail.com>
2020-07-29 01:51:50 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <p>Orgulhosamente criado durante a Imersão React da Alura</p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
